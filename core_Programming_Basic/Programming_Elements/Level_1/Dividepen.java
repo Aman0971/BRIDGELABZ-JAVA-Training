c0aa9763f564c671ba54de1dce1b04ba7f629d68
Aman0971 <amanchaudhary4252@gmail.com>
2026-04-29 17:08:27 +0530

    [Aman] adding divide pen problem

---
+++ core_Programming_Basic/Programming_Elements/Level_1/Dividepen.java
@@ -0,0 +1,9 @@
+public class Dividepen{
+	public static void main(String[] args) {
+		int student =  3;
+		int pen =  14;
+		int findpen =  pen/student;
+		int remainder =  pen%student;
+		System.out.print("The Pen Per Student is "+ findpen + " and the remaining pen not distributed is "+ remainder);
+	}
+}
