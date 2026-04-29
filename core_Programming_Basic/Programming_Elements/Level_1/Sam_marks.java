ad881c5cb50d78184abaebabdf12d2476216c1fb
Aman0971 <amanchaudhary4252@gmail.com>
2026-04-29 15:59:45 +0530

    [Aman Chaudhary]Added a java program(convertDistance)

--- core_Programming_Basic/Programming_Elements/Level_1/Sam_marks.java
+++ core_Programming_Basic/Programming_Elements/Level_1/Sam_marks.java
@@ -1,11 +1,10 @@
-public class Ques_2{
+public class Sam_marks{
 	public static void main(String[] args) {
 		int math= 94;
 		int phy = 95;
 		int chem =96;
 		int  total= 100;
 		int avg = (math+phy+chem)/3;
-		System.out.print("Sam’s average mark in PCM is"+avg);
-		
+		System.out.print("Sam’s average mark in PCM is"+ avg);
 	}
 }
---
+++ core_Programming_Basic/Programming_Elements/Level_1/convertDistance.java
@@ -0,0 +1,8 @@
+public class convertDistance{
+	public static void main(String[] args) {
+		double kilometer = 10.8;
+		double miles = kilometer * 1.6;
+		
+		System.out.print("The distance 10.8 km in miles is "+ miles);
+	}
+}
