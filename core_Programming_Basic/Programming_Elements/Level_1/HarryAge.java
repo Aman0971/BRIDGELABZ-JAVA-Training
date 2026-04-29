b24283f4b9fa66b87dc28a33b566033bb710fcb6
Aman0971 <amanchaudhary4252@gmail.com>
2026-04-29 14:48:52 +0530

    Added Java program (HarryAge)

---
+++ core_Programming_Basic/Programming_Elements/Level_1/HarryAge.java
@@ -0,0 +1,8 @@
+public class HarryAge{
+	public static void main(String[] args) {
+		int currentYear = 2024;
+		int birthYear = 2000;
+		int age  = currentYear-birthYear;
+		System.out.print("Harry's age in 2024 is" +age);
+	}
+}
