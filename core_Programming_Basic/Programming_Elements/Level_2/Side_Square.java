d2b0eeee5a3a74bf0255ac94874fbab6641bbf4f
Aman0971 <amanchaudhary4252@gmail.com>
2026-05-04 17:00:56 +0530

    [Aman] adding a  program(find side)

---
+++ core_Programming_Basic/Programming_Elements/Level_2/Side_Square.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Side_Square{
+	public static void main(String[] args) {
+		Scanner sc=  new Scanner(System.in);
+		double perimeter = sc.nextDouble();
+		double side = perimeter/4;
+		System.out.print("one side of Square is: "+side);
+	}
+}
