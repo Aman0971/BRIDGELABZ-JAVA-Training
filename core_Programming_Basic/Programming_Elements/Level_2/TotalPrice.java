a0f7cbe73736c8dfce777ce81d5c4ce72aa97477
Aman0971 <amanchaudhary4252@gmail.com>
2026-05-04 17:01:48 +0530

    [Aman] adding a program to calculate price

---
+++ core_Programming_Basic/Programming_Elements/Level_2/TotalPrice.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class TotalPrice{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double unitPrice = sc.nextDouble();
+		double quantity = sc.nextDouble();
+		double totalPrice = unitPrice*quantity;
+		System.out.print("The total purchase price is INR "+totalPrice+" if the quantity "+quantity+
+				" and unit price is INR "+unitPrice);
+	}
+}
