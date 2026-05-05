c135e7dcca9ec61c466039e1baf833ea7cf7a1b6
Aman0971 <amanchaudhary4252@gmail.com>
2026-05-05 12:45:42 +0530

    [Aman] adding Factorial program

---
+++ core_Programming_Basic/Programming_Elements/ControlFlow_Programs/Level_1/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Factorial{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum1 = 1;
+		if(n <= 0) {
+			System.out.print("Number is not Positive Integer");
+		}
+		else {
+			int temp = n;
+			while(temp>0) {
+				sum1  *= temp;
+				temp--;
+			}
+		System.out.print(sum1);
+		}
+	}
+}
