6d52e12b8d8946f5a42bc34a888bd65eaafe3b81
Aman0971 <amanchaudhary4252@gmail.com>
2026-05-05 11:29:28 +0530

    [Aman] sum of natural number program

---
+++ core_Programming_Basic/Programming_Elements/ControlFlow_Programs/Level_1/SumOfNaturalNum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class SumOfNaturalNum{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int sum = 0;
+		if(a>0) {
+			sum = (a*(a+1))/2;
+			System.out.print("The sum of "+a+" natural numbers is "+sum);
+		}
+		else {
+			
+		
+		System.out.print("The number "+a+" is not a natural number");
+		}
+	}
+}
