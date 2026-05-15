8ff2fe85aa03a72ec5be87346c6046c962d8994b
Aman0971 <amanchaudhary4252@gmail.com>
2026-05-15 22:23:52 +0530

    [Aman] add a program

---
+++ core_programming_Intermediate/Programming_Elements/Array/Level_2/WeightStatus.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class WeightStatus{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		double weight[] = new double[n];
+		double height[] = new double[n];
+		String[] status = new String[n];
+		double bmi[] = new double[n];
+		
+		for(int i = 0; i<n; i++){
+			weight[i] = sc.nextDouble();
+			height[i] = sc.nextDouble();
+		}
+		
+		for(int i=0; i<n; i++) {
+			bmi[i] =  weight[i]/(height[i]*height[i]);
+			
+			if(bmi[i]<= 18.4) {
+				status[i] ="Underweight";
+			}
+			else if (bmi[i] >= 18.5 && bmi[i] <= 24.9) {
+                status[i] = "Normal";
+            }
+            else if (bmi[i] >= 25.0 && bmi[i] <= 39.9) {
+                status[i] = "Overweight";
+            }
+            else {
+                status[i] = "Obese";
+            }
+		}
+		for(int i =0; i<n; i++) {
+			System.out.println("Weigth: "+ weight[i] + " kg");
+			System.out.println("height: "+height[i]);
+			System.out.println("bmi : "+bmi[i]);
+			System.out.println("status : "+status[i]);
+		}
+		
+	}
+}
