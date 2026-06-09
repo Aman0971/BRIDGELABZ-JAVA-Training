4efc6f4da59434a2d2c984b9be75ce8428eae500
Aman0971 <amanchaudhary4252@gmail.com>
2026-06-09 14:36:00 +0530

    [Aman]add a design principle program

---
+++ Design_Modeling/Design_Principles/Kiss.java
@@ -0,0 +1,9 @@
+package Design_Principles;
+public class Kiss{
+      static void setEmail(){
+        System.out.print("Email set successfully");
+      }
+    public static void main(String[] args){
+        setEmail();
+    }
+}
